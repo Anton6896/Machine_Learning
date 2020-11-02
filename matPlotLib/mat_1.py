ae2f1c5894748ef462c5ecf62215ba4ece04f3be
Anton.R <Anton-r@ive.com>
2020-11-02 15:08:10 +0500

    ff

--- matPlotLib/mat_1.py
+++ matPlotLib/mat_1.py
@@ -18,30 +18,45 @@ def base():
         f"this is x : {x} \nthis is y : {y}"
     )
     # plt.plot(x, y)
-    plt.xlabel('x label')
-    plt.ylabel('y label')
-    plt.title('my title')
-
-    plt.subplot(1, 2, 1)
-    # plt.plot(x, y, 'r')
-    plt.subplot(1, 2, 2)
+    # plt.xlabel('x label')
+    # plt.ylabel('y label')
+    # plt.title('my title')
+    #
+    # plt.subplot(1, 2, 1)
+    # # plt.plot(x, y, 'r')
+    # plt.subplot(1, 2, 2)
     # plt.plot(y, x, 'b')
 
     # oop method
+    # fig = plt.figure()
+    # # size of fig [0.1, 0.1, 0.8, 0.8]
+    # axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+    # # .plot() will fill the fig
+    # # axes.plot(x, y)
+    # axes.set_xlabel('x label')
+    # axes.set_ylabel('y label')
+    # axes.set_title('title my ')
+
+    # subplot
     fig = plt.figure()
-    # size of fig [0.1, 0.1, 0.8, 0.8]
-    axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
-    # ,plot() will fill the fig
-    axes.plot(x, y)
-    axes.set_xlabel('x label')
-    axes.set_ylabel('y label')
-    axes.set_title('title my ')
+    axes1 = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+    axes2 = fig.add_axes([0.57, 0.15, 0.3, 0.2])
+    axes1.plot(x, y)
+    axes2.plot(y, x)
+
+
+def adv_1():
+    x = np.linspace(0, 5, 11)
+    y = x ** 2
+
+    fig, axes = plt.subplots(nrows=1, ncols=2)
+    for axe in axes:  # axes is an list
+        axe.plot(x, y)
 
 
 if __name__ == '__main__':
-    path = str(pathlib.Path(
-        __file__).parent.parent.absolute())
+    # base()
+    adv_1()
 
-    print(f" thi file path :  {path}/matPlotLib/mat_1.py")
-    base()
-    plt.show()
+    plt.tight_layout()  # visual
+    plt.show()  # print
