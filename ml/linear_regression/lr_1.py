d31ea4b195c3ceb82e513568dfc4e0da9ea58c2b
ant_pc <anton-r@live.com>
2020-11-05 16:52:24 +0500

    fff

---
+++ ml/linear_regression/lr_1.py
@@ -0,0 +1,15 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matPlotLib.pyplot as plt
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    plt.tight_layout()  # visual
+    plt.show()  # print
