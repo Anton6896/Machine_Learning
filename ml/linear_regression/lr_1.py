5a4420d494adb97eb50d25d03bc6f505e409c29c
ant_pc <anton-r@live.com>
2020-11-05 17:30:49 +0500

    d

--- ml/linear_regression/lr_1.py
+++ ml/linear_regression/lr_1.py
@@ -4,15 +4,16 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-import os
-if not os.path.exists("ml/linear_regression/images"):
-    os.mkdir("ml/linear_regression/images")
+# import os
+# if not os.path.exists("ml/linear_regression/images"):
+#     os.mkdir("ml/linear_regression/images")
 
 
 def main():
     df = pd.read_csv(
         '/mnt/d/Documents/Code/Mashine_Learning/course_data/11-Linear-Regression/USA_Housing.csv')
-    print(df.head())
+    print(df.info())
+    sns.displot(df['Price'], kde=True)
 
 
 if __name__ == "__main__":
