679a86691d2ac7fd688517c72c9731db839e7999
Mac <Anton-r@live.com>
2020-12-02 14:30:48 +0500

    ff

--- ml/linear_regression/lr_1.py
+++ ml/linear_regression/lr_1.py
@@ -1,22 +1,20 @@
-from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-# import os
-# if not os.path.exists("ml/linear_regression/images"):
-#     os.mkdir("ml/linear_regression/images")
-
-
-def main():
-    df = pd.read_csv(
-        '/mnt/d/Documents/Code/Mashine_Learning/course_data/11-Linear-Regression/USA_Housing.csv')
-    print(df.info())
-    sns.displot(df['Price'], kde=True)
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
 
 
 if __name__ == "__main__":
-    main()
-    plt.tight_layout()  # visual
-    plt.show()  # print
+    df = pd.read_csv(
+        # mac path
+        '/Users/ant/Documents/python/ml/course_data/11-Linear-Regression/USA_Housing.csv')
+    print(df.info())
+
+    # sns.displot(df['Price'], kde=True)
+    # sns.heatmap(df.corr(), annot=True)
+
+    print(df.columns)
+    plt.show()  # show picture
